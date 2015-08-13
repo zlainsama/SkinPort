dc8442dc2e80acb86f07b7960d6844821cc5824d
Lain MultipleInstances <zlainsama@163.com>
2015-08-13 17:07:47 +0800

    SkinCustomization

--- src/main/java/lain/mods/skinport/SkinPortRenderPlayer.java
+++ src/main/java/lain/mods/skinport/SkinPortRenderPlayer.java
@@ -26,6 +26,36 @@ public class SkinPortRenderPlayer extends RenderPlayer
         modelPlayer = (SkinPortModelPlayer) mainModel;
     }
 
+    public void doRender(AbstractClientPlayer p_76986_1_, double p_76986_2_, double p_76986_4_, double p_76986_6_, float p_76986_8_, float p_76986_9_)
+    {
+        boolean smHeadwear = modelPlayer.bipedHeadwear.showModel;
+        boolean smLeftLegwear = modelPlayer.bipedLeftLegwear.showModel;
+        boolean smRightLegwear = modelPlayer.bipedRightLegwear.showModel;
+        boolean smLeftArmwear = modelPlayer.bipedLeftArmwear.showModel;
+        boolean smRightArmwear = modelPlayer.bipedRightArmwear.showModel;
+        boolean smBodyWear = modelPlayer.bipedBodyWear.showModel;
+        boolean smCloak = modelPlayer.bipedCloak.showModel;
+
+        int flags = SkinPort.clientCache.getUnchecked(p_76986_1_.getUniqueID());
+        modelPlayer.bipedHeadwear.showModel = SkinCustomization.contains(flags, SkinCustomization.hat);
+        modelPlayer.bipedLeftLegwear.showModel = SkinCustomization.contains(flags, SkinCustomization.left_pants_leg);
+        modelPlayer.bipedRightLegwear.showModel = SkinCustomization.contains(flags, SkinCustomization.right_pants_leg);
+        modelPlayer.bipedLeftArmwear.showModel = SkinCustomization.contains(flags, SkinCustomization.left_sleeve);
+        modelPlayer.bipedRightArmwear.showModel = SkinCustomization.contains(flags, SkinCustomization.right_sleeve);
+        modelPlayer.bipedBodyWear.showModel = SkinCustomization.contains(flags, SkinCustomization.jacket);
+        modelPlayer.bipedCloak.showModel = SkinCustomization.contains(flags, SkinCustomization.cape);
+
+        super.doRender(p_76986_1_, p_76986_2_, p_76986_4_, p_76986_6_, p_76986_8_, p_76986_9_);
+
+        modelPlayer.bipedHeadwear.showModel = smHeadwear;
+        modelPlayer.bipedLeftLegwear.showModel = smLeftLegwear;
+        modelPlayer.bipedRightLegwear.showModel = smRightLegwear;
+        modelPlayer.bipedLeftArmwear.showModel = smLeftArmwear;
+        modelPlayer.bipedRightArmwear.showModel = smRightArmwear;
+        modelPlayer.bipedBodyWear.showModel = smBodyWear;
+        modelPlayer.bipedCloak.showModel = smCloak;
+    }
+
     @Override
     protected ResourceLocation getEntityTexture(AbstractClientPlayer player)
     {
@@ -33,6 +63,36 @@ public class SkinPortRenderPlayer extends RenderPlayer
         if (!skin.isSkinReady())
             skin = SkinProviderAPI.getDefaultSkin(player);
         return skin.getSkinLocation();
+    }
+
+    protected void renderEquippedItems(AbstractClientPlayer p_77029_1_, float p_77029_2_)
+    {
+        boolean smHeadwear = modelPlayer.bipedHeadwear.showModel;
+        boolean smLeftLegwear = modelPlayer.bipedLeftLegwear.showModel;
+        boolean smRightLegwear = modelPlayer.bipedRightLegwear.showModel;
+        boolean smLeftArmwear = modelPlayer.bipedLeftArmwear.showModel;
+        boolean smRightArmwear = modelPlayer.bipedRightArmwear.showModel;
+        boolean smBodyWear = modelPlayer.bipedBodyWear.showModel;
+        boolean smCloak = modelPlayer.bipedCloak.showModel;
+
+        int flags = SkinPort.clientCache.getUnchecked(p_77029_1_.getUniqueID());
+        modelPlayer.bipedHeadwear.showModel = SkinCustomization.contains(flags, SkinCustomization.hat);
+        modelPlayer.bipedLeftLegwear.showModel = SkinCustomization.contains(flags, SkinCustomization.left_pants_leg);
+        modelPlayer.bipedRightLegwear.showModel = SkinCustomization.contains(flags, SkinCustomization.right_pants_leg);
+        modelPlayer.bipedLeftArmwear.showModel = SkinCustomization.contains(flags, SkinCustomization.left_sleeve);
+        modelPlayer.bipedRightArmwear.showModel = SkinCustomization.contains(flags, SkinCustomization.right_sleeve);
+        modelPlayer.bipedBodyWear.showModel = SkinCustomization.contains(flags, SkinCustomization.jacket);
+        modelPlayer.bipedCloak.showModel = SkinCustomization.contains(flags, SkinCustomization.cape);
+
+        super.renderEquippedItems(p_77029_1_, p_77029_2_);
+
+        modelPlayer.bipedHeadwear.showModel = smHeadwear;
+        modelPlayer.bipedLeftLegwear.showModel = smLeftLegwear;
+        modelPlayer.bipedRightLegwear.showModel = smRightLegwear;
+        modelPlayer.bipedLeftArmwear.showModel = smLeftArmwear;
+        modelPlayer.bipedRightArmwear.showModel = smRightArmwear;
+        modelPlayer.bipedBodyWear.showModel = smBodyWear;
+        modelPlayer.bipedCloak.showModel = smCloak;
     }
 
     @Override
